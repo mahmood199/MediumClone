9ebb3969e15192339f26554c9cd4aa2869d10c51
mahmood199 <gaudentas199@gmail.com>
2021-08-24 02:39:18 +0530

    Entire UI ready and minor changes

--- app/src/main/java/com/example/mediumclone/Intro1Fragment.kt
+++ app/src/main/java/com/example/mediumclone/Intro1Fragment.kt
@@ -1,4 +1,21 @@
 package com.example.mediumclone
 
-class Intro1Fragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class Intro1Fragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+
+
 }
--- app/src/main/java/com/example/mediumclone/MainActivity.kt
+++ app/src/main/java/com/example/mediumclone/MainActivity.kt
@@ -9,32 +9,13 @@ import androidx.drawerlayout.widget.DrawerLayout
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var drawer_layout_main_activity: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val toolbar: Toolbar = findViewById(R.id.toolbar_main_activity)
-        drawer_layout_main_activity = findViewById(R.id.drawer_layout_main_activity)
-
-        val actionBarDrawerToggle = ActionBarDrawerToggle(
-            this,
-            drawer_layout_main_activity,
-            R.string.navigation_drawer_open,
-            R.string.navigation_drawer_close
-        )
-
-        drawer_layout_main_activity.addDrawerListener(actionBarDrawerToggle)
-        actionBarDrawerToggle.syncState()
 
     }
 
-    override fun onBackPressed() {
-        if (drawer_layout_main_activity.isDrawerOpen(GravityCompat.START)) {
-            drawer_layout_main_activity.closeDrawer(GravityCompat.END)
-        } else {
-            super.onBackPressed()
-        }
-    }
+
 }
